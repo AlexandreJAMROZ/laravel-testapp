828bd79c99b8fb4f3b7388a13f2afe6a134e617b
AlexandreJAMROZ <alexandre@helium.nc>
2025-10-23 11:38:10 +1100

    Modify styles in .css and tailwind classes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/style.css')
+    @vite('resources/css/style.css') <!-- Tailwind est importé ici -->
     <title>@yield('title', 'Mon site')</title>
 </head>
-<body class="min-h-screen flex flex-col">
+<body>
 
+    <div id="app" class="bg-gray-100 min-h-screen flex flex-col">
     {{-- HEADER --}}
-    <header class="header">
+    <header class="header bg-white mb-8 shadow-md">
         <nav class="nav">
             <div class="nav_container">
-                <ul class="mb-8 flex w-full divide-x divide-gray-300">
+                <ul class="flex w-full divide-x divide-gray-300">
                     <li class="flex w-full hover:bg-gray-100 cursor-pointer">
                         <a class="flex-1 text-center py-5" href="/">Accueil</a>
                     </li>
@@ -20,7 +21,7 @@
                         <a class="flex-1 text-center py-5" href="/posts">Posts</a>
                     </li>
                     <li class="flex w-full hover:bg-gray-100 cursor-pointer">
-                        <a class="flex-1 text-center py-5" href="/contact">Nous contacter</a>
+                        <a class="flex-1 text-center py-5" href="/contact">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -29,18 +30,19 @@
 
     {{-- CONTENU DE LA PAGE --}}
     <main class="main flex-1">
-        <div class="page">
+        <div class="main_container">
             @yield('content')
         </div>
     </main>
 
     {{-- FOOTER --}}
-    <footer class="footer bg-gray-100 text-center p-4">
+    <footer class="footer bg-white text-center p-4">
         <div class="container">
-            <p class="text-lg font-normal text-gray-500 dark:text-gray-400 text-center p-4 bg-gray-100">
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400 text-center p-4">
                 &copy; 2025 Alexandre JAMROZ. Tous droits réservés.
             </p>
         </div>
     </footer>
+    </div>
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,16 +4,24 @@
 
 @section('content')
 <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
-    Lists of posts
+    Latest posts
 </h1>
-@foreach ($post_list as $post)
-    <div class="post mb-8">
-        <h2 class="text-3xl font-semibold text-gray-700 dark:text-white text-center">{{ $post->title }}</h2>
-        <ul>
-            <li><p class="text-lg font-normal text-gray-500 dark:text-gray-400 text-center">{{ $post->content }}</p></li>
-            <li><p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400 text-center">Écrit par : {{ $post->author }}</p></li>
-        </ul>
-    </div>
-@endforeach  
+
+<div class="posts">
+    @foreach ($post_list as $post)
+        <div class="post mb-8 rounded-xl flex bg-white w-11/12 mx-auto">
+            <div class="w-3/4 p-4">
+                <h2 class="mb-2 text-3xl font-semibold text-gray-700 dark:text-white">{{ $post->title }}</h2>
+                <p class="text-lg font-medium text-gray-500 dark:text-gray-400">Écrit par : {{ $post->author }}</p></li>
+            </div>
+
+            <div class="w-1/4 p-4 flex justify-center items-center">
+                <a href="posts/{{ $post->id }}" class="text-xl font-medium bg-gray-200 p-2 rounded-xl">
+                    Détails
+                </a>
+            </div>
+        </div>
+    @endforeach 
+</div>
         
 @endsection
